d86056b904ec4ad388bec3f4d7c4cf7817ef164d
rahul <rahulyadavv2011@gmail.com>
2026-05-20 10:39:57 +0530

    feat(appointment-service): implement CQRS-based appointment module with cached patient read model
    - setup appointment microservice and DB connectivity
    
    - add appointment entity, repository, service, and controller layers
    
    - implement cachedPatient read model synced from patient-service
    
    - add DTOs and validation exception handling
    
    - seed appointment database

---
+++ appointment-service/src/main/java/com/pm/appointmentservice/entity/CachedPatient.java
@@ -0,0 +1,51 @@
+package com.pm.appointmentservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "cached_patient")
+public class CachedPatient {
+
+    @Id
+    private UUID id;
+    private String fullName;
+    private String email;
+    private Instant UpdatedAT;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Instant getUpdatedAT() {
+        return UpdatedAT;
+    }
+
+    public void setUpdatedAT(Instant updatedAT) {
+        UpdatedAT = updatedAT;
+    }
+}
